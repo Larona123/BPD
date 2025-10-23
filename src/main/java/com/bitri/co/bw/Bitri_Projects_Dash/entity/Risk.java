40298f8c63755ff087a77fe7de33b6925936002a
Phenyo V. Ramodimo <pramodimo@bitri.co.bw>
2025-10-23 10:20:07 +0200

    API testing done

--- src/main/java/com/bitri/co/bw/Bitri_Projects_Dash/entity/Risk.java
+++ src/main/java/com/bitri/co/bw/Bitri_Projects_Dash/entity/Risk.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -19,7 +21,15 @@ public class Risk {
     private Long id;
 
     private String description;
+
     private String severity;
+
+    private String owner;
+
+    @Column(name = "date_raised")
+    private LocalDate dateRaised;
+
+    @Column(name = "mitigation_plan")
     private String mitigationPlan;
 
     @ManyToOne
